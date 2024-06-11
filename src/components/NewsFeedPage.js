f2c2e820c8c8002f32e972a0b3cdb1ad36655965
jilla pavan <jillapavan22001@gmail.com>
2024-06-11 17:38:44 +0530

    intermediate commit

--- src/components/NewsFeedPage.js
+++ src/components/NewsFeedPage.js
@@ -1,37 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import NewsCard from "./NewsCard";
-import { newsDataAPI } from "../utils/constants";
+import { fetcher, newsDataAPI_URL } from "../utils/constants";
 import Shimmer from "./Shimmer";
+import useSWR from "swr";
 
 function NewsFeedPage() {
-  const [newsData, setNewsData] = useState([]);
+  // Using useSWR to fetch data from an API
+
+  const { data, error } = useSWR(newsDataAPI_URL, fetcher);
+
+  // code related to navigate to next card
+
   const [activeNewsCard, setActiveNewsCard] = useState(0);
-
   const handleNextClick = () => {
-    setActiveNewsCard((activeNewsCard + 1) % newsData.length);
+    setActiveNewsCard((activeNewsCard + 1) % data.articles.length);
   };
 
-  // Fetching the data using API
+  // Adding efficient User Experience by implementing Shimmer, Error....
 
-  const fetchNewsData = async () => {
-    const fetchData = await fetch(newsDataAPI);
-    const json = await fetchData.json();
-    setNewsData(json.articles);
-  //  console.log(json.articles);
-  };
+  if (error) return <div>{error}</div>;
 
-  useEffect(() => {
-    fetchNewsData();
-  }, []);
-
-  return newsData.length === 0 ? (
+  return !data ? (
     <Shimmer /> // using shimmer effect
   ) : (
-    <div className=" bg-slate-500 w-[300px] h-[500px] border border-black rounded-2xl ">
+    <div className=" bg-slate-500 w-[300px] h-[500px] border border-black rounded-2xl">
       <div className="p-2 flex justify-center items-center flex-col">
         <h1 className="m-1 font-bold text-white">News Feed App</h1>
 
-        {newsData.map((x, i) => {
+        {/* using data that is fetched */}
+
+        {data.articles.map((x, i) => {
           return (
             <div className={activeNewsCard === i ? "block" : "hidden"}>
               <NewsCard newsData={x} key={i} />
@@ -39,8 +37,10 @@ function NewsFeedPage() {
           );
         })}
 
+        {/* implementing swipe feature so to navigate to next feed */}
+
         <button
-          className="m-2 px-6 py-2 bg-green-300 rounded-lg"
+          className="m-2 px-6 py-2 bg-green-300"
           onClick={handleNextClick}
         >
           Next
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,7 +1,7 @@
 export const imgNotFound =
   "https://lightwidget.com/wp-content/uploads/localhost-file-not-found.jpg";
 
-export const newsDataAPI =
+export const newsDataAPI_URL =
   "https://newsapi.org/v2/top-headlines?country=in&apiKey=25405f8e540947ac943d372472eade78";
 
- 
+export const fetcher = (url) => fetch(url).then((res) => res.json());
